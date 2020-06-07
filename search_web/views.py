b0b295383308e24f009634c9f724fc66f777088b
maciektr <mtratnowiecki@gmail.com>
2020-06-07 23:35:48 +0200

    Simple front

--- search_web/views.py
+++ search_web/views.py
@@ -1,13 +1,21 @@
-from flask import render_template
+from flask import current_app, request, render_template
 from datetime import datetime
 
 
 def register_views(app):
     app.context_processor(base_context)
     app.add_url_rule('/', view_func=index)
+    app.add_url_rule('/search/', view_func=search, methods=['POST'])
 
 
 def index():
+    return render_template('index.html')
+
+
+def search():
+    query = request.form['query']
+    print('Query:', query)
+    print(current_app.search(query))
     return render_template('index.html')
 
 
