d41873d30bf6a8f1629f40e2097125624d30b7f4
maciektr <mtratnowiecki@gmail.com>
2020-06-07 19:19:37 +0200

    Add sources for cars on wikipedia

--- config.py
+++ config.py
@@ -4,5 +4,46 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 STATIC_FOLDER = "/static"
 
-# PAGES_FOLDER = 'wiki'
-PAGES_FOLDER = 'test_dir'
+PAGES_FOLDER = 'wiki_pages'
+PAGES_SOURCE = [
+    # Cars by make
+    'https://en.wikipedia.org/wiki/List_of_Chrysler_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Citro%C3%ABn_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_DeSoto_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Dodge_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Ford_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_GAC_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Geo_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Hyundai_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Holden_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Honda_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Iran_Khodro_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Jeep_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Kia_Motors_automobiles',
+    'https://en.wikipedia.org/wiki/List_of_Lamborghini_automobiles',
+    'https://en.wikipedia.org/wiki/List_of_Lexus_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Lincoln_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Maserati_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Mazda_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Mercedes-Benz_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Mercury_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Nissan_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Opel_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Peugeot_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Proton_car_models',
+    'https://en.wikipedia.org/wiki/List_of_Plymouth_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Pontiac_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Renault_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Saab_passenger_cars',
+    'https://en.wikipedia.org/wiki/SEAT#Current_model_range',
+    'https://en.wikipedia.org/wiki/List_of_Subaru_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Toyota_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Vauxhall_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Volkswagen_passenger_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Volvo_passenger_cars',
+    'https://en.wikipedia.org/wiki/List_of_Chevrolet_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Cadillac_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Buick_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_BMW_vehicles',
+    'https://en.wikipedia.org/wiki/List_of_Audi_vehicles'
+]
