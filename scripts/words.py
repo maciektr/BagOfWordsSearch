e7cb431d725d2809e88a912193eb123232cdaeb2
maciektr <mtratnowiecki@gmail.com>
2020-06-07 17:42:59 +0200

    Simple bag of words search engine

--- scripts/words.py
+++ scripts/words.py
@@ -21,6 +21,7 @@ class WordsProcess:
         self.folder_path = os.path.abspath(folder_path)
         self.read_pages()
         self.get_all_terms()
+        self._N_PAGES_TO_RET = 5
 
     def all_pages_paths(self):
         return [os.path.join(self.folder_path, filename) for filename in os.listdir(self.folder_path)]
@@ -87,9 +88,8 @@ class WordsProcess:
         for i in range(len(terms)):
             self.term_id[terms[i]] = i
 
-    def get_page_vector(self, page):
+    def get_vector_from_words(self, words):
         result = lil_matrix((len(self.terms), 1))
-        words = self.pages[page]
         bag = {}
         for word in words:
             if word in bag:
@@ -98,9 +98,15 @@ class WordsProcess:
                 bag[word] = 1
         for key, val in sorted(bag.items()):
             if key not in self.term_id:
+                if key not in self.terms:
+                    continue
                 self.term_id[key] = self.terms.index(key)
             result[self.term_id[key]] = val
-        return page, result
+        return result
+
+    def get_page_vector(self, page):
+        words = self.pages[page]
+        return page, self.get_vector_from_words(words)
 
     def get_all_vectors(self):
         if self.vectors is not None:
@@ -118,22 +124,65 @@ class WordsProcess:
         vectors = [val for key, val in sorted(self.vectors.items(), key=lambda x:x[0])]
         return hstack(vectors)
 
+    def idf_norm(self, matrix):
+        n_docs = len(self.pages.keys())
+        idf = np.log(n_docs / np.sum(matrix > 0, axis=1))
+        idf_mat = np.array([idf, ] * matrix.shape[1]).T
+        return matrix * idf_mat
+
     def get_matrix(self):
         if self.matrix is not None:
             return self.matrix
-        self.matrix = lil_matrix(self.stack_vectors())
-        n_docs = len(self.pages.keys())
-        for col in range(self.matrix.shape[1]):
-            self.matrix[:, col] = self.matrix[:, col] * np.log(n_docs / np.sum(self.matrix[:,col] > 0))
+        self.matrix = lil_matrix(self.stack_vectors()).toarray()
+        self.matrix = self.idf_norm(self.matrix)
         return self.matrix
+
+    def get_all_pages(self):
+        return sorted(self.pages.keys())
+
+    def get_bag_from_queries(self, query):
+        words = list(filter(lambda w: len(w) > 1, map(WordsProcess.process_word, query.split())))
+        return self.get_vector_from_words(words)
+
+    @staticmethod
+    def norm(matrix):
+        return matrix / np.linalg.norm(matrix, axis=0)
+
+    def search(self, query, n_pages=5):
+        query = WordsProcess.norm(self.get_bag_from_queries(query).toarray())
+        self.matrix = WordsProcess.norm(self.get_matrix())
+        correlation = []
+        for col in range(self.matrix.shape[1]):
+            print("Dot", np.sum(np.dot(self.matrix[:, col], query)))
+            correlation.append(np.dot(self.matrix[:, col], query)[0])
+            # print(self.matrix[:, col])
+            # print(query)
+        # correlation = [np.dot(np.array(self.matrix[:, col]), query) for col in range(self.matrix.shape[1])]
+        print(correlation)
+
+        # ids = np.argpartition(correlation, -self._N_PAGES_TO_RET)[-self._N_PAGES_TO_RET:]
+        ids = np.argpartition(correlation, -n_pages)[-n_pages:]
+        pages = self.get_all_pages()
+        return [pages[i] for i in ids]
 
 
 if __name__ == '__main__':
     # folder = '../wiki'
+    # folder = '../tiny2'
     folder = '../test_dir'
     proc = WordsProcess(folder)
-    # print(proc.get_all_vectors())
-    print(proc.get_matrix())
+
+    # print(proc.get_all_terms())
+    # vect = proc.get_all_vectors()
+    # for k,v in vect.items():
+    #     print("V", k, '\n', v)
+
+    # print("Matrix")
+    # print(proc.get_matrix())
+    # print(proc.get_matrix().shape)
+    # print(proc.get_bag_from_queries("Panno Litwo"))
+
+    print(proc.search("four wheel drive", n_pages=5))
 
     # _, a = proc.get_page_vector('Asian_Century')
     # print(np.sum(a), a)
